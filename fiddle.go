68cea025d56bfa421cabd13a9b1cb5f5e701fd86
Calder Coalson <caldercoalson@gmail.com>
2013-09-24 19:04:32 -0500

    Better printing.

--- fiddle.go
+++ fiddle.go
@@ -3,6 +3,7 @@ package fiddle
 import "bytes"
 import "encoding/hex"
 import "strconv"
+import "strings"
 
 /********************
 ***   Bits Type   ***
@@ -17,22 +18,32 @@ type Bits struct {
 ***   Constructors   ***
 ***********************/
 
-func FromByte (dat byte) *Bits {
-    return &Bits{[]byte{dat}, 8}
+func FromByte (b byte) *Bits {
+    return &Bits{[]byte{b}, 8}
 }
 
-func FromBytes (dat []byte) *Bits {
-    return &Bits{dat, 8*len(dat)}
+func FromBytes (b []byte) *Bits {
+    return &Bits{b, 8*len(b)}
 }
 
-func FromHex (dat string) *Bits {
-    b, e := hex.DecodeString(dat)
+func FromBin (s string) *Bits {
+    s = strings.Replace(s, " ", "", -1)
+    l := (len(s)+7) / 8
+    b := &Bits{make([]byte, l), len(s)}
+    for i := 0; i < len(s); i++ {
+        if s[i] == '1' { b.dat[i/8] |= 1 << uint(7-i%8) }
+    }
+    return b
+}
+
+func FromHex (s string) *Bits {
+    b, e := hex.DecodeString(s)
     if e != nil { panic(e) }
     return FromBytes(b)
 }
 
-func FromUnicode (dat string) *Bits {
-    b := []byte(dat)
+func FromUnicode (s string) *Bits {
+    b := []byte(s)
     return &Bits{b, 8*len(b)}
 }
 
@@ -48,10 +59,11 @@ func (bits *Bits) String () string {
     s := ""
     for i := 0; i < len(bits.dat); i++ {
         if i > 0 { s += " " }
-        s += strconv.FormatInt(int64(bits.dat[i]), 2)
+        for j := uint(0); i*8+int(j)<bits.len; j++ {
+            if bits.dat[i] >> (7-j) & 1 == 0 { s += "0" } else { s += "1" }
+        }
     }
-    chop := 8 - bits.len%8
-    return s[:len(s)-chop]
+    return s
 }
 
 func (bits *Bits) HexString () string {
@@ -101,10 +113,21 @@ func (bits *Bits) FromTo (start int, end int) *Bits {
 ********************/
 
 func (bits *Bits) Equal (other *Bits) bool {
-    return bytes.Equal(bits.dat, other.dat)
+    return bytes.Equal(bits.dat, other.dat) && bits.len == other.len
 }
 
 func (bits *Bits) Plus (other *Bits) *Bits {
+    // // Byte splicing
+    // b := &Bits{append(bits.dat, other.dat...), bits.len + other.len}
+
+    // // Bit shifting
+    // shift := uint(start % 8)
+    // if shift > 0 {
+    //     for i := len(); i < len(b.dat)-1; i++ {
+    //         b.dat[i] = (b.dat[i] << shift) | (b.dat[i+1] >> uint(8-shift))
+    //     }
+    //     b.dat[len(b.dat)-1] = b.dat[len(b.dat)-1] << shift
+    // }
     return &Bits{append(bits.dat, other.dat...), bits.len + other.len}
 }
 
@@ -128,6 +151,14 @@ func (bits *Bits) Unicode () string {
     return string(bits.dat)
 }
 
+/***************************
+***   Decoding Methods   ***
+***************************/
+
+// func (bits *Bits) Chunks (num int) chunks []*Bits, err error {
+    
+// }
+
 /******************
 ***   Private   ***
 ******************/
@@ -138,4 +169,10 @@ func min (x int, y int) int {
 
 func max (x int, y int) int {
     if x > y { return x } else { return y }
+}
+
+func parseBinByte (b string) byte {
+    x, e := strconv.ParseUint(b, 2, 8)
+    if e != nil { panic(e) }
+    return byte(x)
 }
